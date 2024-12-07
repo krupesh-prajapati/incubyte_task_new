e5774b294efef7b86e689c9558d4839eaf97377f
krupesh <krupesh.prajapati@remedoapp.com>
2024-12-07 22:38:57 +0530

    Updated function to get regex as a split string & calculate

--- stringCalculator.js
+++ stringCalculator.js
@@ -3,9 +3,21 @@ function stringCalculator(str) {
 
     if (!str.length) return 0;
 
-    let delimeter = ',';
+    let delimeter = [',']
 
-    const arr = str.split(delimeter);
+    if (str.charAt(0) == '/' && str.charAt(1) == '/') {
+        if (str.charAt(2) == '[') {
+            const match = str.match(/\[(.*?)\]/g);
+            const results = match.map(match => match.replace(/[\[\]]/g, ''));
+            delimeter = results;
+        }
+        else {
+            delimeter[0] = (str.charAt(2));
+        }
+    }
+
+    const regexp = new RegExp(`${delimeter.join('|')}|\n`, "g")
+    const arr = str.split(regexp);
     let nagativeNums = [];
     let res = 0;
 
