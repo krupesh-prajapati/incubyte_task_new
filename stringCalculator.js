cb6d3433104287e661a711d8eafb8185968d3871
krupesh <krupesh.prajapati@remedoapp.com>
2024-12-07 22:32:05 +0530

    added basic function to calulate on static delimeter

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,8 +1,25 @@
 
 function stringCalculator(str) {
-    
+
     if (!str.length) return 0;
 
+    let delimeter = ',';
+
+    const arr = str.split(delimeter);
+    let nagativeNums = [];
+    let res = 0;
+
+    arr.map((ele, idx) => {
+        if (!Number.isNaN(parseInt(ele))) {
+            if (parseInt(ele) < 0) {
+                nagativeNums.push(ele)
+            } else {
+                res += parseInt(ele)
+            }
+        }
+    })
+
+    return nagativeNums.length ? `negative numbers not allowed ${nagativeNums}` : res;
 }
 
 
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -5,4 +5,16 @@ describe('stringCalculator', () => {
         expect(stringCalculator("")).toBe(0);
     });
 
+    test('should add numbers separated by commas', () => {
+        expect(stringCalculator("1,2,3")).toBe(6);
+    });
+
+    test('should return error message for negative numbers', () => {
+        expect(stringCalculator("1,-2,3")).toBe("negative numbers not allowed -2");
+    });
+
+    test('should list all negative numbers in error message', () => {
+        expect(stringCalculator("-1,-2,3")).toBe("negative numbers not allowed -1,-2");
+    });
+
 });
